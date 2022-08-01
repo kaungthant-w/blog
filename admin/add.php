adc667b691f5145f316d3c9bc098e5bc4e06023a
Kaung Thant <kaungthant9513@gmail.com>
2022-08-01 16:23:50 +0630

    spread header.php and footer.php at index.php

---
+++ admin/add.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+require("../config/config.php");
+
+if(empty($_SESSION["user_id"]) && empty($_SESSION['logged_in'])) {
+  header("Location:login.php");
+}
+
+include "header.php";
+
+?>
+
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <?php
+
+              ?>
+              <!-- /.card-header -->
+              <div class="card-body">
+                
+              </div>
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+<?php
+include "footer.php";
+?>
--- admin/index.php
+++ admin/index.php
@@ -1,98 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Blog</title>
+<?php
+session_start();
+require("../config/config.php");
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../dist/css/adminlte.min.css">
-</head>
-<body class="hold-transition sidebar-mini">
-<div class="wrapper">
+if(empty($_SESSION["user_id"]) && empty($_SESSION['logged_in'])) {
+  header("Location:login.php");
+}
 
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-    </ul>
+include "header.php";
 
-    <!-- SidebarSearch Form -->
-    <li class="form-inline ml-3">
-      <div class="input-group input-group-sm" >
-        <input class="form-control form-control-navbar" type="search" placeholder="Search">
-        <div class="input-group-append">
-          <button class="btn btn-sidebar btn-default" type="submit">
-            <i class="fas fa-search"></i>
-          </button>
-        </div>
-      </div>
-    </li>
+?>
 
-  </nav>
-  <!-- /.navbar -->
-
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index.html" class="brand-link">
-      <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
-        </div>
-      </div>
-
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Blogs
-              </p>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Starter Page</h1>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
 
     <!-- Main content -->
     <div class="content">
@@ -101,8 +18,16 @@
           <div class="col-md-12">
             <div class="card">
               <div class="card-header">
-                <a href="#" class="btn btn-success">New Blog Post</a>
+                <a href="add.php" class="btn btn-success">New Blog Post</a>
               </div>
+
+              <?php
+                $stmt = $pdo -> prepare("SELECT * FROM posts ORDER BY id DESC");
+                $stmt -> execute();
+                $result = $stmt -> fetchAll();
+
+                // print_r($result);
+              ?>
               <!-- /.card-header -->
               <div class="card-body">
                 <table class="table table-bordered">
@@ -115,19 +40,32 @@
                     </tr>
                   </thead>
                   <tbody>
+                    <?php
+
+                      if ($result) {
+
+                        $i = 1;
+                        foreach($result as $value ) { ?>
+
                     <tr>
-                      <td>1.</td>
-                      <td>Title Text</td>
+                      <td><?php echo $i; ?></td>
+                      <td><?php echo $value["title"]; ?></td>
                       <td>
-                        content text
+                      <?php echo substr($value["content"],0,50); ?>
                       </td>
                       <td>
                         <div class="btn-group">
-                          <a href="" class="btn btn-warning">Edit</a>
-                          <a href="" class="btn btn-danger">Delete</a>
+                          <a href="edit.php?id=<?php echo $value['id'] ?>" class="btn btn-warning">Edit</a>
+                          <a href="delete.php?id=<?php echo $value['id'] ?>" class="btn btn-danger">Delete</a>
                         </div>
                       </td>
                     </tr>
+
+                      <?php
+                      $i++;
+                        }
+                      }
+                    ?>
                   </tbody>
                 </table>
               </div>
@@ -161,26 +99,6 @@
     </div>
   </aside>
   <!-- /.control-sidebar -->
-
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      <a href="logout.php" class="btn btn-default">Logout</a>
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2022 <a href="https://adminlte.io">A Programmer</a>.</strong> All rights reserved.
-  </footer>
-</div>
-<!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-
-<!-- jQuery -->
-<script src="../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../dist/js/adminlte.min.js"></script>
-</body>
-</html>
+<?php
+include "footer.php";
+?>
